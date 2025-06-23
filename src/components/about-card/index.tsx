884ee00fce5703bf6636b49b272b3f2df9274508
LeonardoAValerio <leonardoaugustovalerio@gmail.com>
2025-06-23 00:05:21 -0300

    fix: add responsive about-cards

--- src/components/about-card/index.tsx
+++ src/components/about-card/index.tsx
@@ -2,9 +2,9 @@ import Image from "next/image"
 
 export function AboutCards({ cards }: { cards: { imgPath: string, title: string, description: string }[]}) {
     return (
-        <ul className="flex items-stretch h-full justify-around">
+        <ul className="flex h-full justify-around flex-wrap gap-8">
             {cards.map((card, index) => {
-                return <li key={index} className="h-full border rounded-3xl w-1/5 flex flex-col items-center justify-around p-4">
+                return <li key={index} className="border rounded-3xl h-180 w-96 flex flex-col items-center justify-around p-8">
                     <Image alt={card.imgPath} src={card.imgPath} width={200} height={200}></Image>
                     <div className="h-1/2 flex flex-col gap-4">
                         <h3 className="text-4xl" >{card.title}</h3>
