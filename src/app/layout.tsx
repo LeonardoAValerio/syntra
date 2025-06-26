d43b92fd7ed2a19168510c165cc1d116e78f832f
LeonardoAValerio <leonardoaugustovalerio@gmail.com>
2025-06-26 00:10:22 -0300

    feat: add google analytics into project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Michroma } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/components/header";
+import Script from "next/script";
 
 const michroma = Michroma({
   weight: "400",
@@ -21,6 +22,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-F7X6QDLT5B"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-F7X6QDLT5B');
+          `}
+        </Script>
+      </head>
       <body
         className={`${michroma.variable} antialiased`}
       >
