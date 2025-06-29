a82c54242259ea8d7da55eb5381df29ff31a1d66
LeonardoAValerio <leonardoaugustovalerio@gmail.com>
2025-06-29 01:54:12 -0300

    feat: create partial login and create user to directly firebase

--- src/app/login/criar/page.tsx
+++ src/app/login/criar/page.tsx
@@ -1,31 +1,67 @@
+'use client';
+
+import { auth } from "@/lib/firebase/firebase";
+import { Message } from "@/lib/types/message";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function CreateUser() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [messages, setMessages] = useState<Message[]>([]);
+
+    const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const localErrors: string[] = [];
+
+        if (password.length < 6) localErrors.push("Senha deve ter mais que 6 caracteres");
+        if (!email.includes("@")) localErrors.push("Email inválido");
+
+        if (localErrors.length > 0) {
+            setMessages(localErrors.map((error) => ({text: error, type: "error"})))
+            return;
+        }
+
+        try {
+            const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+            console.log("Criou a conta:", userCredential);
+            setMessages([{text: "Usuário criado com sucesso. Vá para a página de login e entre no sistema", type: "success"}]); // limpa erros após sucesso
+        } catch (e) {
+            setMessages([{text: "Email já cadastrado", type: "error"}]);
+        }
+    };
+
     return (
-        <main className="w-screen h-screen bg-primary-black flex items-center justify-start">
-            <div className="w-1/4 h-full bg-second-black p-8">
-                <form className="gap-8 flex flex-col text-primary-white">
+        <main className="w-screen h-screen flex items-center justify-start">
+            <div className="w-full md:w-1/2 h-full flex flex-col bg-second-black p-8 gap-12">
+                <form className="gap-8 flex flex-col text-primary-white" onSubmit={handleLogin}>
                     <div className="items-center flex w-full">
-                        <h2 className="text-3xl">Criar sua conta</h2>
+                        <h2 className="text-3xl">Criar uma nova conta</h2>
                     </div>
                     <div className="flex flex-col gap-2 p-2">
-                        <label>Email</label>
-                        <input type="email" className="border-b p-2"></input>
+                        <label htmlFor="email" >Email</label>
+                        <input id="email" type="email" className="border-b p-2" onChange={(e) => setEmail(e.target.value)}></input>
                     </div>
                     <div className="flex flex-col gap-2 p-2">
-                        <label>Senha</label>
-                        <input type="password" className="border-b p-2"></input>
+                        <label htmlFor="password" >Senha</label>
+                        <input id="password" type="password" className="border-b p-2" onChange={(e) => setPassword(e.target.value)}></input>
                     </div>
                     <div className="flex justify-between">
                         <div className="bg-primary-white text-primary-black rounded-3xl h-8 w-24 flex items-center justify-center cursor-pointer">
-                            <button className="hover:pointer cursor-pointer" type="submit">Criar</button>
+                            <button className="hover:pointer cursor-pointer" type="submit" >Criar</button>
                         </div>
                         <div className="text-primary-white h-8 flex items-center justify-center">
                             <button><Link href={"/login"}>Login ⇾</Link></button>
                         </div>
                     </div>
                 </form>
+
+                <ul>{messages.map((message, index) => {
+                    return <li key={index} >{message.text}</li> 
+                })}</ul>
             </div>
+            <div className="bg-primary-black hidden md:block w-full h-full"></div>
         </main>
     )
 }
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,24 +1,64 @@
+'use client';
+
+import { auth } from "@/lib/firebase/firebase";
+import { GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup, OAuthProvider } from "firebase/auth";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+        try {
+            e.preventDefault();
+            const userCredential = await signInWithEmailAndPassword(auth, email, password);
+
+            console.log("Deu certo");
+        } catch (e) {
+            console.log("Deu errado: ", e);
+        }
+    }
+
+    const handleGoogleLogin = async () => {
+        const provider = new GoogleAuthProvider();
+        try {
+            const result = await signInWithPopup(auth, provider);
+            const token = await result.user.getIdToken();
+            console.log("Token Firebase:", token);
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    const handleMicrosoftLogin = async () => {
+        const provider = new OAuthProvider("microsoft.com");
+        try {
+            const result = await signInWithPopup(auth, provider);
+            const credential = OAuthProvider.credentialFromResult(result);
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
     return (
-        <main className="w-screen h-screen bg-primary-black flex items-center justify-start">
-            <div className="w-1/4 h-full flex flex-col bg-second-black p-8 gap-12">
-                <form className="gap-8 flex flex-col text-primary-white">
+        <main className="w-screen h-screen flex items-center justify-start">
+            <div className="w-full md:w-1/2 h-full flex flex-col bg-second-black p-8 gap-12">
+                <form className="gap-8 flex flex-col text-primary-white" onSubmit={handleLogin}>
                     <div className="items-center flex w-full">
                         <h2 className="text-3xl">Login</h2>
                     </div>
                     <div className="flex flex-col gap-2 p-2">
-                        <label>Email</label>
-                        <input type="email" className="border-b p-2"></input>
+                        <label htmlFor="email" >Email</label>
+                        <input id="email" type="email" className="border-b p-2" onChange={(e) => setEmail(e.target.value)}></input>
                     </div>
                     <div className="flex flex-col gap-2 p-2">
-                        <label>Senha</label>
-                        <input type="password" className="border-b p-2"></input>
+                        <label htmlFor="password" >Senha</label>
+                        <input id="password" type="password" className="border-b p-2" onChange={(e) => setPassword(e.target.value)}></input>
                     </div>
                     <div className="flex justify-between">
                         <div className="bg-primary-white text-primary-black rounded-3xl h-8 w-24 flex items-center justify-center cursor-pointer">
-                            <button className="hover:pointer cursor-pointer" type="submit">Entrar</button>
+                            <button className="hover:pointer cursor-pointer" type="submit" >Entrar</button>
                         </div>
                         <div className="text-primary-white h-8 flex items-center justify-center">
                             <button><Link href={"/login/criar"}>Criar nova conta ⇾</Link></button>
@@ -28,23 +68,20 @@ export default function Login() {
 
                 <section className="flex flex-col gap-4">
                     <div className="flex items-center justify-center gap-4">
-                        <button
+                        <button onClick={handleGoogleLogin}
                             className="flex items-center gap-2 bg-primary-white border px-3 py-3 rounded-full shadow cursor-pointer"
                             >
                             <img src="/google-icon.svg" alt="Google" className="w-6 h-6" />
                         </button>
-                        <button
+                        <button onClick={handleMicrosoftLogin}
                             className="flex items-center gap-2 bg-primary-white border px-3 py-3 rounded-full shadow cursor-pointer"
                             >
                             <img src="/microsoft-icon.svg" alt="Microsot" className="w-6 h-6" />
                         </button>
-                        <button
-                            className="flex items-center gap-2 bg-primary-white border px-3 py-3 rounded-full shadow cursor-pointer"
-                            >
-                            <img src="/yahoo-icon.svg" alt="Yahoo" className="w-6 h-6" />
-                        </button>
                     </div>
                 </section>
+            </div>
+            <div className="bg-primary-black hidden md:block w-full h-full">
             </div>
         </main>
     )
